d162de65ce00da856a79a4963897a107e1f95422
WindKies <kekhongten21@gmail.com>
2023-10-13 00:18:53 +0700

    13/10/2023/

---
+++ app/components/ClientOnly.tsx
@@ -0,0 +1,29 @@
+'use client';
+
+import { useEffect, useState } from "react";
+
+interface ClientOnlyProps {
+    children: React.ReactNode;
+}
+
+const ClientOnly: React.FC<ClientOnlyProps> = ({
+    children
+}) => { 
+    const [hasMounted, setHasMouted] = useState(false);
+
+    useEffect(() => {
+        setHasMouted(true);
+    }, []);
+
+    if (!hasMounted){
+        return null;
+    }
+
+    return (
+        <>
+            {children}
+        </>
+    );
+}
+
+export default ClientOnly;
